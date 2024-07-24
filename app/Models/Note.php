8908b92df507e4f4dfd9d014333aff533328d1f8
Sudip Das <sudip5428@gmail.com>
2024-07-24 11:47:27 +0530

    code commit for send notes app

---
+++ app/Models/Note.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Note extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $guarded = [
+        'id',
+    ];
+
+    protected $casts = [
+        'is_published' => 'boolean',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
